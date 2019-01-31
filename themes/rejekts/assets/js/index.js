b1ce6f5cadebe55219da6f8bde6bfc0c65cab670
olgatymofieieva <tigercub220@gmail.com>
2019-01-31 18:06:05 +0200

    new2

--- themes/rejekts/assets/js/index.js
+++ themes/rejekts/assets/js/index.js
@@ -10,7 +10,10 @@
 
 
 (function(){
-  $('.timer__wrapper ').countdown('2019/05/18 08:30:00', function(event) {
+  
+  const finalDate = Date.parse('2019-05-18T08:30:00+02:00');
+
+  $('.timer__wrapper ').countdown(finalDate, function(event) {
     $('.timer__month').html(event.strftime(`%m`));
     $('.timer__week').html(event.strftime('%W'));
     $('.timer__days').html(event.strftime('%d'));
