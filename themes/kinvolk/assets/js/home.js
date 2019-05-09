98a46f561db0afbde83231d1412dca9edd0d5531
olgatymofieieva <tigercub220@gmail.com>
2019-05-09 13:58:19 +0300

    Update lokomotive

---
+++ themes/kinvolk/assets/js/home.js
@@ -0,0 +1,41 @@
+function initLokomotiveSlider() {
+  return new Swiper ('#lokomotive-slider', {
+    slidesPerView: 2,
+    spaceBetween: 20,
+    grabCursor: true,
+    allowTouchMove: true,
+
+    pagination: {
+      el: '#lokomotive-slider__pagination'
+    },
+
+    on: {
+      resize: function () {
+        if(window.innerWidth > 767) {
+          this.destroy(false, true);
+        }
+
+        this.update();
+      },
+    },
+
+    breakpoints: {
+      576: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        grabCursor: true,
+        allowTouchMove: true
+      }
+    }
+  });
+}
+
+if(window.innerWidth < 768) {
+  initLokomotiveSlider();
+}
+
+window.onresize = function() {
+  if(window.innerWidth < 768) {
+    initLokomotiveSlider();
+  }
+}
